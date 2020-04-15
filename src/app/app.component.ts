d73f8016e2983efaa980beebe066a767573075a5
Dmitry Nikolaev <ndimon777@gmail.com>
2020-04-15 17:51:30 +0300

    Chat implementation started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Question and Answer';
+  title = 'Question and Answer. Bot v. 0.0.1';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { DatatoenterComponent } from './datatoenter/datatoenter.component';
 import { EntereddataComponent } from './entereddata/entereddata.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatatoenterComponent,
-    EntereddataComponent
+    EntereddataComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
